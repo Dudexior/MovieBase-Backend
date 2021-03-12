b63d00d127971fd7aa06bc21f0b0615087e1e003
dudexior <dawid@tomczakd.pl>
2021-03-12 22:01:58 +0100

    configured model builder using FluentAPI

--- DAL/MovieBaseContext.cs
+++ DAL/MovieBaseContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DAL
@@ -14,5 +15,30 @@ namespace DAL
         }
 
         public DbSet<Movie> Movies { get; set; }
+        public DbSet<Display> Displays { get; set; }
+        public DbSet<SourceType> SourceTypes { get; set; }
+        public DbSet<Movie> MovieImages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Display>()
+                .Property(p => p.SourceTypeId).HasConversion<int>();
+
+            modelBuilder.Entity<SourceType>()
+                .Property(p => p.SourceTypeId).HasConversion<int>();
+
+            modelBuilder
+           .Entity<SourceType>().HasData(
+               Enum.GetValues(typeof(SourceTypeId))
+                   .Cast<SourceTypeId>()
+                   .Select(e => new SourceType()
+                   {
+                       SourceTypeId = e,
+                       Name = e.ToString()
+                   })
+           );
+
+        }
+
     }
 }
