0fe5caf93907d7b514e3d5d74358edb0bf13d1ad
dudexior <dawid@tomczakd.pl>
2021-03-12 20:57:09 +0100

    added basic Movie model

---
+++ DAL/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Models
+{
+    class Movie
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+    }
+}
