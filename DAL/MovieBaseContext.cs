f609673ecfa44cc7a89ecf31979492b5461e1b08
dudexior <dawid@tomczakd.pl>
2021-03-12 20:59:36 +0100

    added DB Context

---
+++ DAL/MovieBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    class MovieBaseContext : DbContext
+    {
+        public MovieBaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
