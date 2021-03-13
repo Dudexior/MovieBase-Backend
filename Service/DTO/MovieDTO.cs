48855ffde693848b115145d4e5cca8781eca5c9e
dudexior <dawid@tomczakd.pl>
2021-03-13 12:30:25 +0100

    created MovieDTO

---
+++ Service/DTO/MovieDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Service.DTO
+{
+    class MovieDTO
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
