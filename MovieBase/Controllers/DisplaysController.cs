ee0d6a9213ffd880cbdee6d5b050f948f7c5a5f9
dudexior <dawid@tomczakd.pl>
2021-03-13 18:45:08 +0100

    created displays controller

---
+++ MovieBase/Controllers/DisplaysController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.DTO;
+using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MovieBase.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DisplaysController : ControllerBase
+    {
+        private readonly IDisplayService _displayService;
+        public DisplaysController(IDisplayService displayService)
+        {
+            _displayService = displayService;
+        }
+
+
+        // GET <DisplaysController>/5
+        [HttpGet("{movieId}")]
+        public IQueryable<DisplayDTO> Get(long movieId, int? top)
+        {
+            return _displayService.GetDisplaysForMovie(movieId, top);
+        }
+    }
+}
