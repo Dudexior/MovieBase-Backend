e9e7b69ad77f836f171d3ad21813cfbf16dc000e
dudexior <dawid@tomczakd.pl>
2021-03-13 12:32:54 +0100

    created DisplayDTO

---
+++ Service/DTO/DisplayDTO.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Models;
+using System;
+
+namespace Service.DTO
+{
+    class DisplayDTO
+    {
+        public long Id { get; set; }
+        public long MovieId { get; set; }
+        public DateTime DisplayDate { get; set; }
+        public SourceTypeId SourceTypeId { get; set; }
+    }
+}
