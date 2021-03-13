cbf9d290f7325501e0c683c46a90d06bc6b56eac
dudexior <dawid@tomczakd.pl>
2021-03-13 12:18:47 +0100

    added basic displays repository

---
+++ Repository/DisplaysRepository.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using DAL.Models;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class DisplaysRepository : IDisplaysRepository
+    {
+        private readonly MovieBaseContext _dbContext;
+        public DisplaysRepository(MovieBaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<Display> GetDisplaysForMovie(long movieId)
+        {
+            return _dbContext.Displays.Where(display => display.Movie.Id == movieId);
+        }
+    }
+}
---
+++ Repository/Interfaces/IDisplaysRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository.Interfaces
+{
+    public interface IDisplaysRepository
+    {
+        IQueryable<Display> GetDisplaysForMovie(long movieId);
+    }
+}
