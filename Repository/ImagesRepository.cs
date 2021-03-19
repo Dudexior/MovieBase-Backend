0b8005e4538fc385078d4fbbf1defa1039849b51
dudexior <dawid@tomczakd.pl>
2021-03-19 10:57:12 +0100

    created images repository

---
+++ Repository/ImagesRepository.cs
@@ -0,0 +1,40 @@
+﻿using DAL;
+using DAL.Models;
+using Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Repository
+{
+    public class ImagesRepository : IImagesRepository
+    {
+        private readonly MovieBaseContext _dbContext;
+        public ImagesRepository(MovieBaseContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IQueryable<MovieImage> getImages()
+        {
+            return _dbContext.MovieImages;
+        }
+
+        public MovieImage InsertImage(MovieImage image)
+        {
+            _dbContext.Add(image);
+            _dbContext.SaveChanges();
+
+            return image;
+        }
+
+        public MovieImage UpdateImage(MovieImage image) 
+        {
+            _dbContext.Update(image);
+            _dbContext.SaveChanges();
+
+            return image;
+        }
+    }
+}
---
+++ Repository/Interfaces/IImagesRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Models;
+using System.Linq;
+
+namespace Repository.Interfaces
+{
+    public interface IImagesRepository
+    {
+        public IQueryable<MovieImage> getImages();
+
+        MovieImage InsertImage(MovieImage image);
+
+        MovieImage UpdateImage(MovieImage image);
+    }
+}
